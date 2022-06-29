ad0202ac6eb8992624e61cc8e626bb991530b434
Mehul-zs <mehul.rawal@zopsmart.com>
2022-06-29 10:38:34 +0530

    Implemented 7 functionalities get, post, delete, and putAuthor

---
+++ library.go
@@ -0,0 +1,425 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Books struct {
+	Id            int    `json:"id"`
+	Title         string `json:"title"`
+	Author        Author `json:"author"`
+	Publication   string `json:"publication"`
+	PublishedDate string `json:"published_date"`
+}
+
+type Author struct {
+	Id        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Dob       string `json:"dob"`
+	PenName   string `json:"pen_name"`
+}
+
+func DbConnect() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := "HelloMehul1@"
+	dbName := "bookstore"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp(127.0.0.1:3306)/"+dbName)
+	//fmt.Println("hello Mehul")
+	if err != nil {
+		panic(err.Error())
+	}
+	//fmt.Println("hello Charlie")
+	return db
+}
+
+func BookExists(book Books, Id int64) bool {
+
+	db := DbConnect()
+	res, err := db.Query("SELECT Id FROM Book WHERE Title = ? AND Publication =? AND  PublsihedDate= ? AND Id =?", book.Title, book.Publication, book.PublishedDate, Id)
+	if err != nil || !res.Next() {
+		return false
+	}
+
+	return true
+}
+
+func GetBook(rw http.ResponseWriter, req *http.Request) {
+	//json.NewEncoder(rw).Encode([]Books{{1, "Mehul", nil, "Penguin", "10/08/2000"}, {2, "Mehul", nil, "Penguin", "10/08/2000"}}) // for manual testing
+	db := DbConnect()
+	title := req.URL.Query().Get("Title")
+	var rows *sql.Rows
+	var err error
+	if title == "" {
+		rows, err = db.Query("select * from Books;")
+	} else {
+		rows, err = db.Query("select * from Books where title=?;", title)
+	}
+	if err != nil {
+		log.Print(err)
+	}
+	books := []Books{}
+	for rows.Next() {
+		book := Books{}
+		err = rows.Scan(&book.Id, &book.Title, &book.Publication, &book.PublishedDate)
+		if err != nil {
+			log.Print(err)
+		}
+
+		books = append(books, book)
+	}
+	json.NewEncoder(rw).Encode(books)
+
+}
+
+func GetBookById(rw http.ResponseWriter, req *http.Request) {
+	db := DbConnect()
+	vars := mux.Vars(req)
+	id, ok := vars["id"]
+	if !ok {
+		fmt.Println("id is missing")
+	}
+	fmt.Println(`id: `, id)
+
+	var rows *sql.Rows
+	var err error
+	if id == "" {
+		rows, err = db.Query("select * from Books;")
+	} else {
+		rows, err = db.Query("select * from Books where id=?;", id)
+
+	}
+	if err != nil {
+		log.Print(err)
+	}
+	books := []Books{}
+	for rows.Next() {
+		book := Books{}
+		err = rows.Scan(&book.Id, &book.Title, &book.Publication, &book.PublishedDate)
+		if err != nil {
+			log.Print(err)
+		}
+
+		books = append(books, book)
+	}
+	json.NewEncoder(rw).Encode(books)
+
+}
+
+func PostByAuthor(rw http.ResponseWriter, req *http.Request) {
+	//json.NewEncoder(rw).Encode([]Books{{1, "Mehul", nil, "Penguin", "10/08/2000"}, {2, "Mehul", nil, "Penguin", "10/08/2000"}}) // for manual testing
+	db := DbConnect()
+	var author *Author
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Print(err)
+		return
+
+	}
+
+	err = json.Unmarshal(body, &author)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	fmt.Println(author.Id)
+	if author.FirstName == "" || author.LastName == "" {
+		rw.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+	//fmt.Println(db.Query("SELECT * FROM Author"))
+	res, err := db.Query("SELECT Id FROM Author WHERE FirstName=? AND LastName=? AND  Dob=? AND PenName=?", author.FirstName, author.LastName, author.Dob, author.PenName)
+	fmt.Println(res.Next())
+	if !res.Next() || err != nil {
+		_, err = db.Exec("INSERT INTO Author (Id, FirstName,LastName, Dob, PenName) VALUES (?, ? , ?, ?, ?)", author.Id, author.FirstName, author.LastName, author.Dob, author.PenName)
+		if err != nil {
+			return
+		}
+		rw.WriteHeader(http.StatusCreated)
+		return
+	}
+	rw.WriteHeader(http.StatusCreated)
+}
+
+func PostByBook(rw http.ResponseWriter, req *http.Request) {
+	db := DbConnect()
+	var Book Books
+	body, err := io.ReadAll(req.Body)
+	//fmt.Println(body)
+	//fmt.Println(Book)
+
+	if err != nil {
+		log.Print(err)
+	}
+	fmt.Println(db)
+	err = json.Unmarshal(body, &Book)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	fmt.Println(Book)
+
+	if Book.Title == "" || Book.Author.FirstName == "" {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if Book.Publication != "Scholastic" && Book.Publication != "Arihant" && Book.Publication != "Penguin" {
+		log.Print("Invalid Publication")
+		return
+	}
+	publishedDate := strings.Split(Book.PublishedDate, "/")
+	if len(publishedDate) < 3 {
+		return
+	}
+	yr, _ := strconv.Atoi(publishedDate[2])
+
+	if yr >= 2022 || yr < 1880 {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var Id int64
+	result, err := db.Query("SELECT Id FROM Author WHERE FirstName = ? AND LastName =? AND  Dob = ? AND PenName =?", Book.Author.FirstName, Book.Author.LastName, Book.Author.Dob, Book.Author.PenName)
+	if err != nil {
+		log.Print(err)
+	}
+	fmt.Println(result)
+	if result.Next() {
+		err = result.Scan(&Id)
+		if err != nil {
+			log.Print("author not present")
+		}
+	} else {
+		res, err := db.Exec("INSERT INTO Author (FirstName,LastName,Dob,PenName) VALUES (? ,?, ?, ?)", Book.Author.FirstName, Book.Author.LastName, Book.Author.Dob, Book.Author.PenName)
+		if err != nil {
+			return
+		}
+		Id, err = res.LastInsertId()
+	}
+	fmt.Println("last, Hello Mehul")
+
+	if !BookExists(Book, Id) {
+		_, err = db.Exec("INSERT INTO Books (Id, Title, Publication,PublishedDate) VALUES (? ,?, ?, ?)", Book.Id, Book.Title, Book.Publication, Book.PublishedDate)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		rw.WriteHeader(http.StatusCreated)
+	}
+	//rw.WriteHeader(http.StatusCreated)
+
+}
+
+func PutBook(rw http.ResponseWriter, req *http.Request) {
+	db := DbConnect()
+	var book Books
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &book)
+	if err != nil {
+		return
+	}
+	if book.Title == "" {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !(book.Publication == "Penguin" || book.Publication == "Arihant" || book.Publication == "Scholastic") {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	publicationDate := strings.Split(book.PublishedDate, "/")
+	if len(publicationDate) < 3 {
+		return
+	}
+	yr, _ := strconv.Atoi(publicationDate[2])
+	if yr > time.Now().Year() || yr < 1880 {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	params := mux.Vars(req)
+	ID, err := strconv.Atoi(params["id"])
+	if ID <= 0 {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	result, err := db.Query("SELECT Id FROM Author WHERE Id = ?", book.Author.Id)
+	if err != nil {
+		log.Print(err)
+	}
+
+	if !result.Next() {
+		log.Print("author not present", book.Author.Id)
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	result, err = db.Query("SELECT * FROM Books WHERE Id = ?", book.Id)
+	if err != nil {
+		log.Print(err)
+	}
+	if !result.Next() {
+		log.Print("Book not present")
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	result, err = db.Query("UPDATE Books SET Title = ? ,Publication = ? ,PublishedDate=?  WHERE id =?", book.Title, book.Publication, book.PublishedDate, book.Id)
+	if err != nil {
+		log.Print(err)
+	}
+
+}
+
+func PutAuthor(rw http.ResponseWriter, req *http.Request) {
+	fmt.Println("Hii")
+	db := DbConnect()
+	var author Author
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	err = json.Unmarshal(body, &author)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if author.FirstName == "" || author.LastName == "" || author.PenName == "" || author.Dob == "" {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Println(author)
+
+	params := mux.Vars(req)
+	ID, err := strconv.Atoi(params["id"])
+	if ID <= 0 {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Println(params)
+	res, err := db.Query("SELECT Id FROM Author WHERE Id = ?", ID)
+	if err != nil {
+		log.Print(err)
+	}
+	if !res.Next() {
+		log.Print("id not present")
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Println("Hello")
+	var id int
+	err = res.Scan(&id)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	_, err = db.Exec("UPDATE Author SET FirstName = ?, LastName = ?, Dob = ? , PenName = ?, Id=?  WHERE Id =?", author.FirstName, author.LastName, author.Dob, author.PenName, ID, ID)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	rw.WriteHeader(http.StatusOK)
+
+}
+
+func DeleteBook(rw http.ResponseWriter, req *http.Request) {
+
+	db := DbConnect()
+	fmt.Println(db)
+	vars := mux.Vars(req)
+	id, err := strconv.Atoi(vars["id"])
+	//ok status ?
+	if err != nil {
+		fmt.Println("Invalid Id")
+	}
+
+	if id <= 0 {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	//
+	_, err = db.Exec("Delete from Book WHERE Id=?", id)
+	if err != nil {
+		log.Print("book not exist")
+	}
+	fmt.Println("Hey")
+}
+
+func DeleteAuthor(rw http.ResponseWriter, req *http.Request) {
+	db := DbConnect()
+	fmt.Println(db)
+	params := mux.Vars(req)
+
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		fmt.Println("Id is missing/Invalid Id")
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Println(reflect.TypeOf(id))
+
+	check, err := db.Prepare("delete from Author WHERE Id=?")
+	if err != nil {
+
+		rw.WriteHeader(http.StatusBadRequest)
+	}
+	res, err := check.Exec(id)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = res.RowsAffected()
+	if err != nil {
+		fmt.Println("last comment!!!")
+	}
+
+}
+
+func main() {
+
+	r := mux.NewRouter()
+	db := DbConnect()
+	fmt.Println(db)
+	r.HandleFunc("/books", GetBook).Methods(http.MethodGet)
+	r.HandleFunc("/books/{id}", GetBookById).Methods(http.MethodGet)
+
+	r.HandleFunc("/author", PostByAuthor).Methods(http.MethodPost)
+	r.HandleFunc("/book", PostByBook).Methods(http.MethodPost)
+
+	r.HandleFunc("/author/{id}", PutAuthor).Methods(http.MethodPut)
+	r.HandleFunc("/books/{id}", PutBook).Methods(http.MethodPut)
+
+	r.HandleFunc("/deleteBook/{id}", DeleteBook).Methods(http.MethodDelete)
+	r.HandleFunc("/deleteAuthor/{id}", DeleteAuthor).Methods(http.MethodDelete)
+	//fmt.Println("Hey Mehul!")
+	//r.HandleFunc("/PutBook")
+
+	Server := http.Server{
+		Addr:    ":5000",
+		Handler: r,
+	}
+
+	fmt.Println("Server started at 5000")
+	log.Fatal(Server.ListenAndServe())
+
+}
---
+++ library_test.go
@@ -0,0 +1,294 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetBook(t *testing.T) {
+	testcases := []struct {
+		desc   string
+		input  *http.Request
+		output []Books
+	}{
+		{"get books", httptest.NewRequest(http.MethodGet, "localhost:8000/book", nil), []Books{{1, "Godin", Author{}, "Penguin", "11/02/1988"}}},
+	}
+
+	for i, tc := range testcases {
+		w := httptest.NewRecorder()
+		GetBook(w, tc.input)
+		//res := w.Result()
+
+		res, _ := io.ReadAll(w.Result().Body)
+		resBooks := []Books{}
+		err := json.Unmarshal(res, &resBooks)
+		if err != nil {
+			return
+		}
+
+		for p := 1; p < len(resBooks); p++ {
+			if resBooks[i] != tc.output[i] {
+				t.Errorf("Error test case failed")
+			}
+		}
+
+	}
+}
+
+func TestGetBookByID(t *testing.T) {
+	testcasesId := []struct {
+		desc    string
+		input2  string
+		output2 Books
+	}{
+		{"get books", "localhost:8000/book/1", Books{1, "Godin", Author{Id: 1}, "Penguin", "12/06/1899"}},
+	}
+
+	for _, tc := range testcasesId {
+		w := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, tc.input2, nil)
+
+		GetBook(w, req)
+		//res := w.Result()
+
+		res, _ := io.ReadAll(w.Result().Body)
+		resBooks := Books{}
+		err := json.Unmarshal(res, &resBooks)
+
+		if err != nil {
+			return
+		}
+
+		if resBooks != tc.output2 {
+			t.Errorf("test caase failed in get by id case")
+		}
+		//fmt.Println(b)
+	}
+
+}
+
+func TestPostByBook(t *testing.T) {
+	testcases := []struct {
+		desc       string
+		postinput  Books
+		postoutput Books
+		status     int
+	}{
+		{"Details", Books{1, "linchpin", Author{}, "Penguin", "18/07/2000"}, Books{3, "Linchpin", Author{}, "Penguin", "18/07/2002"}, 200},
+		{desc: "Invalid publication", postinput: Books{Id: 2, Title: "RD", Author: Author{}, Publication: "NA", PublishedDate: "12/03/2002"}, postoutput: Books{}, status: http.StatusBadRequest},
+	}
+
+	for i, tc := range testcases {
+		rw := httptest.NewRecorder()
+		body, _ := json.Marshal(tc)
+		req := httptest.NewRequest(http.MethodPost, "localhost:8000/Book/", bytes.NewReader(body))
+
+		PostByBook(rw, req)
+
+		//defer rw.Result().Body.Close()
+
+		if rw.Result().StatusCode != tc.status {
+			t.Errorf("%v test case failed at %v, with status : %v", i, tc.desc, tc.status)
+		}
+
+		res, _ := io.ReadAll(rw.Result().Body)
+		resBook := Books{}
+		json.Unmarshal(res, &resBook)
+
+		if resBook != tc.postoutput {
+			t.Errorf("test case failed at %v : %v", i, tc.desc)
+		}
+
+	}
+
+}
+
+func TestPostByAuthor(t *testing.T) {
+	testcases := []struct {
+		desc       string
+		postinput  Author
+		postoutput Author
+		status     int
+	}{
+		{"Valid details", Author{FirstName: "RD", LastName: "Sharma", Dob: "2/11/1989", PenName: "Sharma"}, Author{1, "RD", "Sharma", "2/11/1989", "Sharma"}, http.StatusOK},
+		{"InValid details", Author{FirstName: "", LastName: "Sharma", Dob: "2/11/1989", PenName: "Sharma"}, Author{}, http.StatusBadRequest},
+		{"Author already exists", Author{FirstName: "RD", LastName: "Sharma", Dob: "2/11/1989", PenName: "Sharma"}, Author{}, http.StatusBadRequest},
+	}
+	for i, tc := range testcases {
+		rw := httptest.NewRecorder()
+		body, _ := json.Marshal(tc.postinput)
+		req := httptest.NewRequest(http.MethodPost, "localhost:8000/Book/", bytes.NewReader(body))
+
+		PostByBook(rw, req)
+
+		//defer rw.Result().Body.Close()
+
+		if rw.Result().StatusCode != tc.status {
+			t.Errorf("%v test case failed at %v, with status : %v", i, tc.desc, tc.status)
+		}
+
+		res, _ := io.ReadAll(rw.Result().Body)
+		resBook := Author{}
+		json.Unmarshal(res, &resBook)
+
+		if resBook != tc.postoutput {
+			t.Errorf("test case failed at %v : %v", i, tc.desc)
+		}
+
+	}
+
+}
+
+func TestPutBookById(t *testing.T) {
+	testcases := []struct {
+		desc      string
+		id        int
+		InputById Books
+		status    int
+	}{{"Details", 1, Books{Title: "Linchpin", Author: Author{}, Publication: "Pengiun", PublishedDate: "11/03/2002"}, 200},
+		{"Invalid Publication", 1, Books{Title: "Rakshit", Author: Author{}, Publication: "Mehul", PublishedDate: "11/03/2002"}, http.StatusBadRequest},
+		{"Published date should be between 1880 and 2022", 1, Books{Title: "Check", Author: Author{}, Publication: "", PublishedDate: "1/1/1870"}, http.StatusBadRequest},
+		{"Published date should be between 1880 and 2022", 1, Books{Title: "James", Author: Author{}, Publication: "", PublishedDate: "1/1/2222"}, http.StatusBadRequest},
+		{"Author should exist", 1, Books{}, http.StatusBadRequest},
+		{"Title can't be empty", 1, Books{Title: "", Author: Author{}, Publication: "", PublishedDate: ""}, http.StatusBadRequest},
+	}
+
+	for _, tc := range testcases {
+
+		b, err := json.Marshal(tc.InputById)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+
+		req := httptest.NewRequest(http.MethodPost, "localhost:8000/book", bytes.NewBuffer(b))
+		w := httptest.NewRecorder()
+		PostByBook(w, req)
+
+		res := w.Result()
+		if res.StatusCode != tc.status {
+			t.Errorf("failed for %v\n", tc.desc)
+		}
+	}
+
+}
+
+func TestPutBook(t *testing.T) {
+	testcases := []struct {
+		desc        string
+		inputMethod string
+		target      string
+		body        Books
+		expected    int
+	}{}
+
+	for _, tc := range testcases {
+
+		ReqBody, err := json.Marshal(tc.body)
+		if err != nil {
+			fmt.Errorf("failed %v\n", err)
+		}
+		req := httptest.NewRequest(tc.inputMethod, "https://localhost:8000/book/{id}"+tc.target, bytes.NewBuffer(ReqBody))
+		req = mux.SetURLVars(req, map[string]string{"id": tc.target})
+		w := httptest.NewRecorder()
+		PutBook(w, req)
+
+		res := w.Result()
+		if res.StatusCode != tc.expected {
+			t.Errorf("failed for %s", tc.desc)
+		}
+		//assert.Equal(t, tc.expected, res.StatusCode)
+	}
+}
+
+func TestPutAuthor(t *testing.T) {
+	testcases := []struct {
+		desc        string
+		inputMethod string
+		target      string
+		author      Author
+		expected    int
+	}{
+		{"updating a existing id", "PUT", "7", Author{1, "shani",
+			"kumar", "30/04/2001", "sk"}, http.StatusCreated},
+		{"inserting a new id", "PUT", "1", Author{10, "stephen",
+			"hawkins", "30/04/2001", "sk"}, http.StatusCreated},
+	}
+
+	for _, tc := range testcases {
+
+		ReqBody, err := json.Marshal(tc.author)
+		if err != nil {
+			fmt.Errorf("failed %v\n", err)
+		}
+		req := httptest.NewRequest(tc.inputMethod, "https://localhost:8000/author/{id}"+tc.target, bytes.NewBuffer(ReqBody))
+		req = mux.SetURLVars(req, map[string]string{"id": tc.target})
+		w := httptest.NewRecorder()
+		PutAuthor(w, req)
+
+		res := w.Result()
+		if res.StatusCode != tc.expected {
+			t.Errorf("failed for %s", tc.desc)
+		}
+		if tc.expected != res.StatusCode {
+			t.Errorf("test case failed")
+		}
+	}
+}
+
+func TestDeleteBook(t *testing.T) {
+	testcases := []struct {
+		desc        string
+		inputMethod string
+		target      string
+		expected    int
+	}{
+		{"valid id", "DELETE", "5", http.StatusNoContent},
+		{"invalid id", "DELETE", "-1", http.StatusBadRequest},
+	}
+
+	for _, tc := range testcases {
+
+		req := httptest.NewRequest(tc.inputMethod, "localhost:8000/deleteBook/{id}"+tc.target, nil)
+		w := httptest.NewRecorder()
+		req = mux.SetURLVars(req, map[string]string{"id": tc.target})
+		DeleteBook(w, req)
+
+		res := w.Result()
+		if res.StatusCode != tc.expected {
+			t.Errorf("failed for %s", tc.desc)
+		}
+
+	}
+}
+
+func TestDeleteAuthor(t *testing.T) {
+	testcases := []struct {
+		desc        string
+		inputMethod string
+		target      string
+		expected    int
+	}{
+		{"valid authorId", "DELETE", "2", http.StatusNoContent},
+		{"invalid authorId", "DELETE", "-2", http.StatusBadRequest},
+	}
+
+	for _, tc := range testcases {
+
+		req := httptest.NewRequest(tc.inputMethod, "localhost:8000/deleteAuthor/{id}"+tc.target, nil)
+		w := httptest.NewRecorder()
+		req = mux.SetURLVars(req, map[string]string{"id": tc.target})
+		DeleteAuthor(w, req)
+
+		res := w.Result()
+		if res.StatusCode != tc.expected {
+			t.Errorf("failed for %s", tc.desc)
+		}
+
+	}
+}
